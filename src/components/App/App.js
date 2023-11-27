c604ac5ec5354dbbc1706b01666f1424d4c2c82c
Conmanz <128855595+Conmanz@users.noreply.github.com>
2023-11-27 18:51:31 -0500

    Project 10 Attempt 1

---
+++ src/components/App/App.js
@@ -0,0 +1,117 @@
+// import logo from "../../logo.svg";
+import "./App.css";
+import Header from "../Header/Header";
+// import ItemCard from "../ItemCard/ItemCard";
+// import WeatherCard from "../WeatherCard/WeatherCard";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { useEffect, useState } from "react";
+import ItemModal from "../ItemModal/ItemModal";
+import { getForecastWeather, parseWeatherData } from "../Util/WeatherApi";
+
+const App = () => {
+  // const weatherTemp = "75F";
+
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+  const [temp, setTemp] = useState(0);
+  const [cityName, setCityName] = useState("");
+
+  const handleCreateModal = () => {
+    setActiveModal("create");
+  };
+
+  const handleCloseModal = () => {
+    setActiveModal("");
+  };
+
+  const handleSelectedCard = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
+
+  useEffect(() => {
+    getForecastWeather().then((data) => {
+      const temperature = parseWeatherData(data);
+      setTemp(temperature);
+      setCityName(data.name);
+    });
+  }, []);
+  // console.log(temp);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape" && activeModal !== "") {
+      setActiveModal("");
+    }
+  };
+
+  const handleClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setActiveModal("");
+    }
+  };
+
+  return (
+    <div onKeyDown={handleKeyDown} tabIndex={0}>
+      <Header onCreateModal={handleCreateModal} location={cityName} />
+      <Main weatherTemp={temp} onSelectCard={handleSelectedCard} />
+      <Footer />
+      {activeModal === "create" && (
+        <ModalWithForm name={"New_Garment"} title={"New Garment"} onClose={handleCloseModal} handleClick={handleClick}>
+          <label className="modal__name-input">
+            Name
+            <input className="modal__name-submit" type="text" name="name" placeholder="Name" minLength="1" maxLength="30" />
+          </label>
+          <label className="modal__image-input">
+            Image
+            <input
+              className="modal__image-submit"
+              type="url"
+              name="link"
+              placeholder="Image URL"
+              minLength="1"
+              maxLength="30"
+            />
+          </label>
+          <p className="weather__text">Select the weather type:</p>
+          <div>
+            <div className="hot__input">
+              <input type="radio" id="hot" value="hot" name="weather-type" />
+              <label for="hot">Hot</label>
+            </div>
+            <div className="warm__input">
+              <input type="radio" id="warm" value="warm" name="weather-type" />
+              <label for="warm">Warm</label>
+            </div>
+            <div className="cold__input">
+              <input type="radio" id="cold" value="cold" name="weather-type" />
+              <label for="cold">Cold</label>
+            </div>
+          </div>
+        </ModalWithForm>
+      )}
+      {activeModal === "preview" && (
+        <ItemModal selectedCard={selectedCard} onClose={handleCloseModal} handleClick={handleClick} />
+      )}
+    </div>
+  );
+};
+
+export default App;
+
+// const Day = {
+//   Cloudy: "dayCloudy.svg",
+//   Fog: "dayFog.svg"
+// };
+
+// const Night = {
+//   Cloudy: "nightCloudy.svg",
+//   Fog: "nightFog.svg"
+// };
+
+// const temp = (weatherState, dayTime) => {
+//   if (weatherState === "Cloudy") {
+//     return (dayTime === "Day" ? Day.Cloudy : Night.Cloudy);
+//   }
+// };
